3684ca115f90ca2ba35836ea2729f09d8c0d6549
Takahiro <tkhr_621@docomo.ne.jp>
2021-01-16 23:16:02 +0900

    fix

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def handle_message(event):
 def handle_follow(event):
    line_bot_api.reply_message(
        event.reply_token,
-       TextSendMessage(text='友達追加ありがとう'))
+       TextSendMessage(text='友達追加ありがとう!'))
 
 
 if __name__ == "__main__":
