2e6d5ac3b9245f36c479193e135689679ea21d48
Takahiro <tkhr_621@docomo.ne.jp>
2021-01-17 17:40:44 +0900

    test

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def callback():
 
  #ホットペッパー検索 
 def search_shop(lat, lng):
-   url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?"
+   url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
    params = {}
    params['key'] = YOUR_HOTPEPPER_API
    params['lat'] = lat
@@ -77,6 +77,8 @@ def handle_location_message(event):
    user_lat = event.message.latitude
    user_longit = event.message.longitude
    shop_result = search_shop(user_lat, user_longit)
+   line_bot_api.reply_message(event.reply_token, messages=shop_result)
+
 
    for shop in shop_result.get("shop"):
       # 店舗画像
@@ -139,15 +141,11 @@ def handle_follow(event):
        TextSendMessage(text='友達追加ありがとう!'))
 
  #テキスト返しプログラム 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-   user_lat = event.message.latitude
-   user_longit = event.message.longitude
-   shop_result = search_shop(user_lat, user_longit)
-
-   line_bot_api.reply_message(
-       event.reply_token,
-       TextSendMessage(text=shop_result))
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#    line_bot_api.reply_message(
+#        event.reply_token,
+#        TextSendMessage(text='位置情報を送ってね!'))
 
 
 if __name__ == "__main__":
