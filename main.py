a306b34c12dc2129f1bd30c727d81b3552777021
Takahiro <tkhr_621@docomo.ne.jp>
2021-01-17 23:55:52 +0900

    test

--- main.py
+++ main.py
@@ -71,6 +71,12 @@ def search_shop(lat, lng):
  #メイン処理
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
+
+   ine_bot_api.reply_message(
+       event.reply_token,
+       TextSendMessage(text=event.message.latitude))
+
+
    # 空のリストを作成
    list = []
 
